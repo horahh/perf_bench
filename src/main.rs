62787f12330ab63b14c4af14b5d2689e3aba3170
Horacio Hidalgo <horacio.m.hidalgo.herrera@intel.com>
2023-11-05 21:31:22 -0600

    Add perf_bench function as pub for a benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 
 use core::time::Duration;
 use std::thread;
+
 fn main() {
-    f1();
-    f2();
-    f3();
-    println!("Hello, world!");
+    perf_bench();
+}
+
+pub fn perf_bench(){
+    for _ in 0..10 {
+        f1();
+        f2();
+        f3();
+    }
 }
 
 fn f1() {
-    thread::sleep(Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(10));
 }
 fn f2() {
-    thread::sleep(Duration::from_millis(200));
+    thread::sleep(Duration::from_millis(20));
 }
 fn f3() {
-    thread::sleep(Duration::from_millis(300));
+    thread::sleep(Duration::from_millis(30));
     f4()
 }
 
 fn f4() {
-    thread::sleep(Duration::from_millis(400));
+    thread::sleep(Duration::from_millis(40));
 }
